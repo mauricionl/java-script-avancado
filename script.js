a002012655c1e2f0e434e4977a7995c6f813e686
mauricionl <mauricionuneslopes@hotmail.com>
2017-10-06 20:07:55 -0300

    Segundo commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade')
+];
+
+document.querySelector('.form').addEventListener('submit', function(event){
+    alert('oi');
+});
+
+console.log(campos);
